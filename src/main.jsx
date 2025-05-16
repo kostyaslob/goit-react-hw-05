af4647116a17da16ab4854a16a0b2f1c157ac2a6
kostyaslob <kostyaslob@gmail.com>
2025-05-16 19:03:25 +0300

    add /pages and /components

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// Імпорт стилів нормалізації
 import "modern-normalize";
 import App from "../src/components/App";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>    
   </React.StrictMode>
 );
