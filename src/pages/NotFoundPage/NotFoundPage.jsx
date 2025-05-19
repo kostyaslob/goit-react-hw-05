b2fb151daeb29b5b2ee60822117abfd4f9ab2ccb
kostyaslob <kostyaslob@gmail.com>
2025-05-19 17:14:50 +0300

    add Home button to NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,8 +1,12 @@
 import css from "./NotFoundPage.module.css"
+import { Link } from "react-router-dom";
 
 export default function NotFoundPage() {
     return (
         <div className={css.notFoundPage}>
+            <nav>
+                <Link to="/"><button>Home</button></Link>
+            </nav>
             <h2>Not Found page</h2>
         </div>
     )
